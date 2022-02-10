5fd229109c06d8736df0bb21741ceb11af7772c7
Matthew-Bain-Projects <matthewbain@protonmail.com>
2022-02-09 20:23:42 -0500

    First Commit

---
+++ CrashCourse_RepeatText/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string text;
+    cout << "Enter Text To Be Repeated: " << endl;
+    cin >> text;
+    cout << text << endl;
+    return 0;
+}
